cb10ff039963d356193f477255105669202810d0
Aditya Gholba <aditya.gholba@gmail.com>
2017-06-20 18:47:48 +0530

    Refactor header.

--- contrib/src/test/java/org/apache/apex/malhar/contrib/imageIO/ImageProcessingTest.java
+++ contrib/src/test/java/org/apache/apex/malhar/contrib/imageIO/ImageProcessingTest.java
@@ -5,7 +5,7 @@
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain imageInBytes copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,7 +15,6 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
 package org.apache.apex.malhar.contrib.imageIO;
 /*
